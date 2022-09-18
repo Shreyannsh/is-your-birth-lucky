13218260611ecfd0aad4064101e228604540ccdf
Shreyansh Tiwari <111145568+Shreyannsh@users.noreply.github.com>
2022-09-18 23:59:46 +0530

    feat: compeleted
    completed styling of the app.
    wrote complete css file

--- app.js
+++ app.js
@@ -3,7 +3,29 @@ var number = document.querySelector("#number");
 var btnCheck = document.querySelector("#btn-check");
 
 function clickHandler(){
-    console.log("clicked");
+    inputDate = date.value;
+    newInputDate = inputDate.replaceAll("-","0");
+    var sum = 0;
+    for (var num of newInputDate) {
+        sum = sum + Number(num);
+    }
+    return sum;
+  }
+  function findBithdateIsLucky() {
+
+    if (date.value && number.value>0) {
+        var sum = clickHandler(date.value);
+
+        if (sum % number.value == 0) {
+            output.innerText= "YAY! Your birthday is lucky!! 😍";
+        } else {
+            output.innerText = "Oops!! Your birthday is not lucky!! 😔";
+        }
+    }
+    else{
+        output.value = "Please enter valid inputs!";
+    }
+
 }
 
-btnCheck.addEventListener("click",clickHandler);
+btnCheck.addEventListener("click",findBithdateIsLucky);
