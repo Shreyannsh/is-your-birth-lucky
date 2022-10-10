d1bc643354d5b1530ef7bcc299747250e07fdc78
Shreyansh Tiwari <111145568+Shreyannsh@users.noreply.github.com>
2022-10-11 00:21:52 +0530

    refactor & style
    refactor:
    function name changed
    comparison symbol changed
    
    style:
    width of input boxes increased

--- app.js
+++ app.js
@@ -5,21 +5,24 @@ var privacy = document.querySelector("#privacy");
 var btnprivacy= document.querySelector("#btn-privacy");
 var output = document.querySelector("#output");
 
-function clickHandler(){
-    inputDate = date.value;
-    newInputDate = inputDate.replaceAll("-","0");
+function calculateSum(){
+    var inputDate = date.value;
+    var newInputDate = inputDate.replaceAll("-","0");
     var sum = 0;
     for (var num of newInputDate) {
         sum = sum + Number(num);
     }
     return sum;
+   
   }
-  function findBithdateIsLucky() {
+
+
+  function findBithdateIsLucky(){
 
     if (date.value && number.value > 0) {
-        var sum = clickHandler(date.value);
+        var sum = calculateSum(date.value);
 
-        if (sum % number.value == 0) {
+        if (sum % number.value === 0) {
             output.innerText= "YAY! Your birthday is lucky!! 😍";
         } else {
             output.innerText = "Oops!! Your birthday is not that lucky!! 😔";
@@ -30,8 +33,8 @@ function clickHandler(){
     }
 
 }
+btnCheck.addEventListener("click", findBithdateIsLucky);
 
 btnprivacy.addEventListener("click", () => {
     privacy.style.display = "none";
 });
-btnCheck.addEventListener("click",findBithdateIsLucky);
